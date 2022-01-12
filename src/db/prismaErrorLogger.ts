4cc17457d558a4e589c183c9bc45cefb9b3c08b6
Mauro Consolani <mfconsolani@gmail.com>
2022-01-12 20:39:51 +0100

    Create function to check if eventual response error is related to prisma and allowing to log the error in console

--- src/db/prismaErrorLogger.ts
+++ src/db/prismaErrorLogger.ts
@@ -8,18 +8,21 @@ const PRISMA_CLIENT_ERROR_TYPES = {
     PrismaClientValidationError: "Validation failure. Missing field or type validation error"
 }
 
-export const prismaClientErrorType = (err: Error | unknown) => {
+const prismaClientErrorType = (err: Error | unknown) => {
     if (err instanceof Prisma.PrismaClientKnownRequestError) {
-        return PRISMA_CLIENT_ERROR_TYPES.PrismaClientKnownRequestError
+        return {PrismaClientKnownRequestError: PRISMA_CLIENT_ERROR_TYPES.PrismaClientKnownRequestError}
     } else if (err instanceof Prisma.PrismaClientUnknownRequestError) {
         return PRISMA_CLIENT_ERROR_TYPES.PrismaClientUnknownRequestError
     } else if (err instanceof Prisma.PrismaClientRustPanicError) {
         return PRISMA_CLIENT_ERROR_TYPES.PrismaClientRustPanicError
     } else if (err instanceof Prisma.PrismaClientInitializationError) {
         return PRISMA_CLIENT_ERROR_TYPES.PrismaClientInitializationError
+    } else if (err instanceof Prisma.PrismaClientValidationError) {
+        return {PrismaClientValidationError: PRISMA_CLIENT_ERROR_TYPES.PrismaClientValidationError}
     } else {
-        return PRISMA_CLIENT_ERROR_TYPES.PrismaClientValidationError
+        return new Error("Unknown error on query engine")
     }
 }
 
 
+export default prismaClientErrorType;
--- src/routes/projects.ts
+++ src/routes/projects.ts
@@ -1,12 +1,11 @@
-import { Router, Request, Response } from 'express';
-import { prisma } from '../db/prismaGlobal';
-import Logger from '../middlewares/winstonLoggerMiddleware';
-import {prismaClientErrorType } from '../db/prismaErrorLogger';
+import { Router, Request, Response } from 'express';
+import { prisma, logPrismaError } from '../db';
+
 
 
 export const projectRouter = Router()
 
-projectRouter.get('/', async (req:Request, res:Response) => {   
+projectRouter.get('/', async (req: Request, res: Response) => {
     try {
         const allProjects = await prisma.project.findMany()
         res.status(200).send(allProjects)
@@ -15,31 +14,27 @@ projectRouter.get('/', async (req:Request, res:Response) => {
     }
 });
 
-projectRouter.post('/', async (req:Request, res:Response) => {   
+projectRouter.post('/', async (req: Request, res: Response) => {
+    const { title, company, body, role, skill, duration, expiresBy, likesCount, location } = req.body
     try {
         const postProject = await prisma.project.create({
             data: {
-                title: "sda",
-                company: "sada",
-                body: "sadaadsa",
-                role: {
-                    create: { role: "AArduino Developerr"}
-                },
-                skill: {
-                    create: { skill: "C+++"}
-                },
-                duration: "3 DAys",
-                expiresBy: "2022-11-25T05:48:55.814Z",
-                likesCount: 4,
-                location: "Arg"
+                title,
+                company,
+                body,
+                role: { connect: { role } },
+                skill: { connect: { skill } },
+                duration,
+                expiresBy,
+                likesCount,
+                location
             }
         })
         res.status(200).send(postProject)
-    } catch (error) {
-        let isPrismaError = prismaClientErrorType(error)
-        Logger.error(error)
-        res.status(404).send({success: false, error: isPrismaError || error})
-    } 
+    } catch (error: any) {
+        const isPrismaError = logPrismaError(error)
+        res.status(404).send({ success: false, error: isPrismaError || error })
+    }
 });
 
 
