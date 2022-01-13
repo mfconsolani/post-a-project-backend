93b983113bb397adf80f46e50f2b4062fef6a6dc
Mauro Consolani <mfconsolani@gmail.com>
2022-01-13 21:49:30 +0100

    Add endpoint to retrieve one project by id

--- src/db/prismaErrorLogger.ts
+++ src/db/prismaErrorLogger.ts
@@ -1,6 +1,10 @@
 import { Prisma } from '@prisma/client';
 
-const PRISMA_CLIENT_ERROR_TYPES = {
+interface prsimaError {
+    [errorName: string]: string
+}
+
+const PRISMA_CLIENT_ERROR_TYPES: prsimaError = {
     PrismaClientKnownRequestError: "Error related to the request - for example, a unique constraint violation.",
     PrismaClientUnknownRequestError: "Error related to a request that is not indexed by the query engine",
     PrismaClientRustPanicError: "Error thrown when the underlying engine crashes and exits with a non-zero exit code. Service must be restarted.",
@@ -8,15 +12,15 @@ const PRISMA_CLIENT_ERROR_TYPES = {
     PrismaClientValidationError: "Validation failure. Missing field or type validation error"
 }
 
-const prismaClientErrorType = (err: Error | unknown) => {
+const prismaClientErrorType = (err: Error | unknown): prsimaError | Error => {
     if (err instanceof Prisma.PrismaClientKnownRequestError) {
         return {PrismaClientKnownRequestError: PRISMA_CLIENT_ERROR_TYPES.PrismaClientKnownRequestError}
     } else if (err instanceof Prisma.PrismaClientUnknownRequestError) {
-        return PRISMA_CLIENT_ERROR_TYPES.PrismaClientUnknownRequestError
+        return {PrismaClientUnknownRequestError: PRISMA_CLIENT_ERROR_TYPES.PrismaClientUnknownRequestError }
     } else if (err instanceof Prisma.PrismaClientRustPanicError) {
-        return PRISMA_CLIENT_ERROR_TYPES.PrismaClientRustPanicError
+        return {PrismaClientRustPanicError: PRISMA_CLIENT_ERROR_TYPES.PrismaClientRustPanicError}
     } else if (err instanceof Prisma.PrismaClientInitializationError) {
-        return PRISMA_CLIENT_ERROR_TYPES.PrismaClientInitializationError
+        return {PrismaClientInitializationError: PRISMA_CLIENT_ERROR_TYPES.PrismaClientInitializationError}
     } else if (err instanceof Prisma.PrismaClientValidationError) {
         return {PrismaClientValidationError: PRISMA_CLIENT_ERROR_TYPES.PrismaClientValidationError}
     } else {
--- src/routes/projects.ts
+++ src/routes/projects.ts
@@ -1,16 +1,32 @@
 import { Router, Request, Response } from 'express';
 import { prisma, logPrismaError } from '../db';
 
-
-
 export const projectRouter = Router()
 
 projectRouter.get('/', async (req: Request, res: Response) => {
     try {
-        const allProjects = await prisma.project.findMany()
-        res.status(200).send(allProjects)
+        const findAllProjects = await prisma.project.findMany()
+        res.status(200).send({ success: true, response: findAllProjects })
     } catch (error) {
-        res.status(404).send(error)
+        const isPrismaError = logPrismaError(error)
+        res.status(404).send({ success: false, error: isPrismaError || error })
+    }
+});
+
+projectRouter.get('/:id', async (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+        const findOneProject = await prisma.project.findUnique(
+            {
+                where: {
+                    id: parseInt(id)
+                }
+            }
+        )
+        res.status(200).send({ success: true, response: findOneProject })
+    } catch (error) {
+        const isPrismaError = logPrismaError(error)
+        res.status(404).send({ success: false, error: isPrismaError || error })
     }
 });
 
@@ -30,7 +46,7 @@ projectRouter.post('/', async (req: Request, res: Response) => {
                 location
             }
         })
-        res.status(200).send(postProject)
+        res.status(200).send({success: true, postProject})
     } catch (error: any) {
         const isPrismaError = logPrismaError(error)
         res.status(404).send({ success: false, error: isPrismaError || error })
