b4a282cfe93da6cb47187efcda9897afd062d89f
Mauro Consolani <mfconsolani@gmail.com>
2022-01-15 20:35:20 +0100

    Add validation function to check that fields to update area actually different from records in DB. Work in progress

--- src/routes/projects.ts
+++ src/routes/projects.ts
@@ -14,11 +14,10 @@ projectRouter.get('/', async (req: Request, res: Response) => {
 });
 
 projectRouter.get('/:id', async (req: Request, res: Response) => {
-    const { id } = req.params;
     try {
         const findOneProject = await prisma.project.findUnique({
             where:{
-                id:parseInt(id)
+                id:parseInt(req.params.id)
             },
             include: {
                 role: true,
@@ -57,29 +56,68 @@ projectRouter.post('/', async (req: Request, res: Response) => {
 
 projectRouter.put('/:id', async (req: Request, res: Response) => {
     const { title, company, body, role, skill, duration, expiresBy, likesCount, location } = req.body
-    const fieldsToUpdate = await prisma.project.findUnique({where:{id:parseInt(req.params.id)}})
-    console.log(fieldsToUpdate)
 
-    try {
-        const postProject = await prisma.project.update({
-            where: {
-                id: parseInt(req.params.id)
-            },
-            data: {
-                title: title || undefined,
-                company: company || undefined,
-                body: body || undefined,
-                role: (!!role ? { connect:  { role: role } } : undefined),
-                skill: (!!skill ? { connect:  { skill: skill } } : undefined),
-                duration: duration || undefined,
-                expiresBy: expiresBy|| undefined,
-                likesCount: likesCount || undefined,
-                location: location || undefined
-            }
-        })
-        res.status(200).send({success: true, postProject})
-    } catch (error: any) {
-        const isPrismaError = logPrismaError(error)
-        res.status(404).send({ success: false, error: isPrismaError || error })
-    }
+    const fieldsToUpdate = await prisma.project.findUnique({
+        where:{
+            id:parseInt(req.params.id)
+        },
+        include: {
+            role: true,
+            skill: true
+        }
+    })
+    // const { id, title, company, body, role, skill, duration, expiresBy, likesCount, location } = fieldsToUpdate
+    // const isUpdateRequired = () => {
+        const requestFields = Object.entries(req.body).sort()
+        // @ts-ignore
+        const dataBaseFields = Object.entries(fieldsToUpdate).sort()
+        // console.log(dataBaseFields.slice(-1)[0])
+
+        // let differece = requestFields.filter((x:any) => dataBaseFields.includes(x))
+        // console.log("difference", differece);
+        let valuesToCompare = dataBaseFields.filter((elem, i) => Object.keys(req.body).includes(elem[0]))
+
+        // {
+        //     let setOfDifferences = {}
+        //     if (elem[i] !== requestFields[i] && elem[1] !== requestFields[i]){
+        //         console.log("elem[i]:", [elem[i]], "------ requestFields[1]:", [requestFields[i]])
+        //         // let newValue =  {[requestFields[0]]: requestFields[1]}
+        //         // @ts-ignore
+        //         Object.assign(setOfDifferences, {[requestFields[0]]: requestFields[1]})
+        //         return
+        //     } 
+        //     return setOfDifferences
+        // })
+        // return difference
+    // }
+    console.log("Fields to compare", Object.values(valuesToCompare).map(el => el[0]))
+    console.log("Values from DB", valuesToCompare)
+    console.log("Values from request", requestFields)
+
+
+    res.send("hola")
+    // console.log(isUpdateRequired())
+    
+    // try {
+    //     const postProject = await prisma.project.update({
+    //         where: {
+    //             id: parseInt(req.params.id)
+    //         },
+    //         data: {
+    //             title: title || undefined,
+    //             company: company || undefined,
+    //             body: body || undefined,
+    //             role: (!!role ? { connect:  { role: role } } : undefined),
+    //             skill: (!!skill ? { connect:  { skill: skill } } : undefined),
+    //             duration: duration || undefined,
+    //             expiresBy: expiresBy|| undefined,
+    //             likesCount: likesCount || undefined,
+    //             location: location || undefined
+    //         }
+    //     })
+    //     res.status(200).send({success: true, postProject})
+    // } catch (error: any) {
+    //     const isPrismaError = logPrismaError(error)
+    //     res.status(404).send({ success: false, error: isPrismaError || error })
+    // }
 });
