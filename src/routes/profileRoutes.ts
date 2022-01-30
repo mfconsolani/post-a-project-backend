496e4ea986af2daae2798978fb679d5e55ec70f4
Mauro Consolani <mfconsolani@gmail.com>
2022-01-30 21:40:03 +0100

    Configure /users/:id endpoint to receive and post one or more skills already in db

--- src/routes/profileRoutes.ts
+++ src/routes/profileRoutes.ts
@@ -1,6 +1,5 @@
 import { Router, Request, Response } from "express";
-import { parse } from "path/posix";
-import { prisma, logPrismaError } from '../db';
+import { prisma } from '../db';
 import Logger from "../middlewares/winstonLoggerMiddleware";
 
 //TODO
@@ -34,7 +33,7 @@ profileRouter.post('/company/:id', async (req:Request, res:Response) => {
         if (findCompany && !findProfile){
             const createProfile = await prisma.companyProfile.create({
                 data: {
-                    companyEmail: email,
+                    company: {connect: {email}},
                     industry: industry,
                     phoneNumber: phone,
                     employees: employees,
@@ -45,8 +44,6 @@ profileRouter.post('/company/:id', async (req:Request, res:Response) => {
 
             res.status(201).send({success: true, message: createProfile})
         } else {
-            console.log(!!findCompany, findCompany)
-            console.log(!!findProfile, findProfile)
             res.status(400).send({success: false, message: "Company not registered or profile already registered"})
         }
 
@@ -63,5 +60,59 @@ profileRouter.put('/company/:id', (req:Request, res:Response) => {
 })
 
 
+profileRouter.post('/user/:id', async (req:Request, res: Response) => {
+    
+    let { id } = req.params
+    const parsedId = parseInt(id)
+    const { email, firstName, lastName, birthday, phone, city, description, country, skills } = req.body
+
+    try {
+        const findUser = await prisma.user.findFirst({
+            where: {
+                id: parsedId,
+                email: email
+            }, select: {
+                id: true,
+                email: true,
+                profileType: true
+            }
+        })        
+        const findProfile = await prisma.userProfile.findFirst(email)
+        let mappedData:any = []
+        const mappedSkills = skills.map((skill:any) => {
+            // let mappedStuff = {skill: skill}
+            mappedData.push({skill: skill})
+            return 
+        })
+
+        console.log("mappedData", mappedData)
+        console.log(skills)
+        // console.log(mappedSkills)
+        if (findUser && !findProfile){
+            const createProfile = await prisma.userProfile.create({
+                data: {
+                    user: {connect: {email: email}},
+                    firstName: firstName,
+                    lastName: lastName,
+                    birthday: birthday,
+                    phoneNumber: phone,
+                    city: city,
+                    country: country,
+                    description: description,
+                    skills: {connect: mappedData.length > 1 ? mappedData : {skill: skills[0]}}
+                }
+            })
+
+            res.status(201).send({success: true, message: createProfile})
+        } else {
+            res.status(400).send({success: false, message: "User not registered or profile already registered"})
+        }
+
+    } catch (err){
+        Logger.error(err)
+        res.status(404).send({success: false, error: err})
+    }
+
+})
 
 export default profileRouter;
