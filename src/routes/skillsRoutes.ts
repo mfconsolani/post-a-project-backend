1aeb9cd6c86a2250351686c634db73473ef3965a
Mauro Consolani <mfconsolani@gmail.com>
2022-03-01 21:14:43 +0100

    Create endpoint to add new skill in DB

--- src/routes/skillsRoutes.ts
+++ src/routes/skillsRoutes.ts
@@ -15,6 +15,20 @@ skillsRouter.get('/', async (req: Request, res: Response) => {
             message: "Error when retrieving list of skills in database", 
             error: err})
     }
-}) 
+})
+
+//TODO
+//Post new skill
+skillsRouter.post('/', async (req: Request, res: Response) => {
+    try {
+        // const listSkills = await prisma.skills.findMany()
+        // res.status(200).send(listSkills)        
+    } catch (err) {
+    //     res.status(400).send({
+    //         success: false, 
+    //         message: "Error when retrieving list of skills in database", 
+    //         error: err})
+    // }
+})
 
 export default skillsRouter
